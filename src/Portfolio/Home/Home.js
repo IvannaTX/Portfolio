c2afafd939a2ab87ca23913f22e8e4c38b9265a0
Ivanna Contreras <ivanna.aws@gmail.com>
2022-04-24 23:15:08 -0500

    Contact

--- src/Portfolio/Home/Home.js
+++ src/Portfolio/Home/Home.js
@@ -33,7 +33,7 @@ const Home = (props) => {
                 </Element>
                 <Element name="contact-scroll">
                     <Contact />
-                    {/* <ContactForm /> */}
+                    <ContactForm />
                 </Element>
                 <Footer />
             </div>
